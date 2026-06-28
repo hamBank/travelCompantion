b93b8444cd2d6198add18d96aea734b1f33a51d3
hamBank <foobie@gmail.com>
2026-06-28 20:38:43 +1000

    more themse

--- frontend/src/themes.js
+++ frontend/src/themes.js
@@ -4,6 +4,9 @@ export const THEMES = [
   { id: 'latte',     name: 'Latte',     dark: false, swatch: '#8839ef' },
   { id: 'nord',      name: 'Nord',      dark: true,  swatch: '#88c0d0' },
   { id: 'rose-pine', name: 'Rosé Pine', dark: true,  swatch: '#ebbcba' },
+  { id: 'midnight',  name: 'Midnight',  dark: true,  swatch: '#00d4ff' },
+  { id: 'desert',    name: 'Desert',    dark: false, swatch: '#c2571a' },
+  { id: 'evergreen', name: 'Evergreen', dark: true,  swatch: '#52c873' },
 ]
 
 export const DEFAULT_THEME = 'mocha'
